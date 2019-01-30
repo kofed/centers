6436f3ad330abcf9f34e455a235e8b99315be80d
konstantin.fedulov <konstantin.fedulov@paragon-software.com>
2019-01-30 09:00:20 +0300

    .

---
+++ contour3d.cpp
@@ -0,0 +1,16 @@
+#include "contour3d.h"
+#include "contour.h"
+#include "opencv2/opencv.hpp"
+
+
+Contour3d::Contour3d(const Contour & contour, const int h){
+	for(auto p2 : contour.getPoints()){
+		points.push_back(Point3_<int>(p2.x, p2.y, h));
+	}
+}
+
+void Contour3d::toYml(FileStorage & yml) const{
+	for(auto p : points){
+		yml << p.x << p.y << p.z;
+	}
+}
