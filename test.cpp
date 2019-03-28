1def1e06738c2c05d5548614edefcc9cb0876377
konstantin.fedulov@paragon-software.com <Konstantin.Fedulov@paragon-software.com>
2019-03-28 18:00:53 +0300

    .

--- test.cpp
+++ test.cpp
@@ -1,5 +1,18 @@
 #include "contours.h"
 
 void test(vector<Contours> & left){
-	for(int i = 0; 
+
+	Mat image = imread("test_image.png");		
+	
+	FFrame frame(image);
+	vector<Contours> left = frame.findContours();
+
+	vector<Contours3d> disparity;
+	vector<Contours> right;
+	for(int i = 0;i<10; ++i ){
+		right.push_back(left[i].diviate(i, 4*i)); 
+	}
+	for(int i = 0; i < 10; ++i){
+		disparity.push_back(right[i].disparity(left[i]));
+	}
 }
